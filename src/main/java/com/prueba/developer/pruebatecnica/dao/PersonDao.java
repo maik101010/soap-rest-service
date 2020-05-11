edd9f4a32a1b9d777fba5d871738ef4288c1bdf2
maik101010 <maiksoft10@gmail.com>
2020-05-11 14:59:35 -0500

    refactor inyección dependencia validate

--- src/main/java/com/prueba/developer/pruebatecnica/dao/PersonDao.java
+++ src/main/java/com/prueba/developer/pruebatecnica/dao/PersonDao.java
@@ -7,30 +7,18 @@ import com.techprimers.spring_boot_soap_example.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.time.Period;
-
 @Component
 public class PersonDao implements IPersonRepository {
     private PersonRepository personRepository;
-    private ValidateDataPerson validateDataPerson;
 
     @Autowired
     public PersonDao(PersonRepository personRepository, ValidateDataPerson validateDataPerson) {
-        this.personRepository = personRepository;
-        this.validateDataPerson = validateDataPerson;
-    }
-
-    public PersonDao(PersonRepository personRepository) {
         this.personRepository = personRepository;
     }
 
     @Override
     public Person save(Person personSoap) {
         Person personMapper = MapperPerson.soapToDto(personRepository.save(MapperPerson.toEntity(personSoap)));
-        Period periodAge= validateDataPerson.getDateWithParameters(personSoap.getBirthDate());
-        Period periodTimeBounding= validateDataPerson.getDateWithParameters(personSoap.getBonding());
-        personMapper.setAge(validateDataPerson.converterToDate(periodAge));
-        personMapper.setTimeBounding(validateDataPerson.converterToDate(periodTimeBounding));
         return personMapper;
     }
 }
--- src/main/java/com/prueba/developer/pruebatecnica/service/PersonServiceSoap.java
+++ src/main/java/com/prueba/developer/pruebatecnica/service/PersonServiceSoap.java
@@ -1,19 +1,30 @@
 package com.prueba.developer.pruebatecnica.service;
 
 import com.prueba.developer.pruebatecnica.dao.IPersonRepository;
+import com.prueba.developer.pruebatecnica.utils.validate.ValidateDataPerson;
 import com.techprimers.spring_boot_soap_example.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.Period;
+
 @Service
 public class PersonServiceSoap {
-    private final IPersonRepository personRepository;
+    private IPersonRepository personRepository;
+    private ValidateDataPerson validateDataPerson;
 
     @Autowired
-    public PersonServiceSoap(IPersonRepository personRepository) {
+    public PersonServiceSoap(IPersonRepository personRepository, ValidateDataPerson validateDataPerson) {
         this.personRepository = personRepository;
+        this.validateDataPerson = validateDataPerson;
     }
+
     public Person saveAndGetPerson(Person personSoap) {
-        return personRepository.save(personSoap);
+        Person person = personRepository.save(personSoap);
+        Period periodAge= validateDataPerson.getDateWithParameters(personSoap.getBirthDate());
+        Period periodTimeBounding= validateDataPerson.getDateWithParameters(personSoap.getBonding());
+        person.setAge(validateDataPerson.converterToDate(periodAge));
+        person.setTimeBounding(validateDataPerson.converterToDate(periodTimeBounding));
+        return person;
     }
 }
